5fba91c6ce48cf7e2b9b24e041a568f20614d7a2
Artem K. Jouravsky <artem@jouravsky.ru>
2022-06-29 21:07:54 +0300

    To get coordinates using ipinfo.io rather then whereami

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+# ipinfo.io/[IP address]?token=98045abb33e482
+ipinfo_token = "" # you may append API token here
 USE_ROUNDED_COORDS = False
 OPENWEATHER_API = ""  # paste API token here
 OPENWEATHER_URL = (
