4b1e8fb4b1ea75d2b70ec70a3d587e1cc2baf915
mbonga <artem@jouravsky.ru>
2022-06-29 21:32:35 +0300

    Update config.py
    fix security leaks

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # ipinfo.io/[IP address]?token=98045abb33e482
 ipinfo_token = "" # you may append API token here
 USE_ROUNDED_COORDS = False
-OPENWEATHER_API = "765d3738da1d4ef39d99e4ca40816b10"  # paste API token here
+OPENWEATHER_API = ""  # paste API token here
 OPENWEATHER_URL = (
     "https://api.openweathermap.org/data/2.5/weather?"
     "lat={latitude}&lon={longitude}&"
